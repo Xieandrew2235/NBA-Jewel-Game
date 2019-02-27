6c967c99f86df723b52e2f59420592c2a8afcd48
Andrew <xie.andrew2235@gmail.com>
2019-02-26 17:21:51 -0800

    sort of finished with the functionality of the game, still going to try to play around with that for a bit to make sure i didnt miss any scenarios. still have some work to do with style.css, such as adding a background image, color, border for each section, and proper spacing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,3 +11,82 @@ $( document ).ready(function(){
     var userTotal= 0; 
     var wins= 0;
     var losses = 0;
+
+  $('#numberWins').text(wins);
+  $('#numberLosses').text(losses);
+
+  function reset(){
+        Random=Math.floor(Math.random()*101+19);
+        console.log(Random)
+        $('#randomNumber').text(Random);
+        num1= Math.floor(Math.random()*11+1);
+        num2= Math.floor(Math.random()*11+1);
+        num3= Math.floor(Math.random()*11+1);
+        num4= Math.floor(Math.random()*11+1);
+        userTotal= 0;
+        $('#finalTotal').text(userTotal);
+        } 
+
+  function victory(){
+  alert("You win!");
+    wins++; 
+    $('#numberWins').text(wins);
+    reset();
+  }
+
+  function loser(){
+  alert ("You lost! Javale McGee is laughing at you!!");
+//   look up how to add an image after prompt of loss
+    losses++;
+    $('#numberLosses').text(losses);
+    reset()
+  }
+
+    $('#one').on ('click', function(){
+      userTotal = userTotal + num1;
+      console.log("New userTotal= " + userTotal);
+      $('#finalTotal').text(userTotal); 
+
+          if (userTotal == Random){
+            victory();
+          }
+          else if ( userTotal > Random){
+            loser();
+          }   
+    })  
+    $('#two').on ('click', function(){
+      userTotal = userTotal + num2;
+      console.log("New userTotal= " + userTotal);
+      $('#finalTotal').text(userTotal); 
+          if (userTotal == Random){
+            victory();
+          }
+          else if ( userTotal > Random){
+            loser();
+          } 
+    })  
+    $('#three').on ('click', function(){
+      userTotal = userTotal + num3;
+      console.log("New userTotal= " + userTotal);
+      $('#finalTotal').text(userTotal);
+
+            if (userTotal == Random){
+            victory();
+          }
+          else if ( userTotal > Random){
+            loser();
+          } 
+    })  
+    $('#four').on ('click', function(){
+      userTotal = userTotal + num4;
+      console.log("New userTotal= " + userTotal);
+      $('#finalTotal').text(userTotal); 
+        
+            if (userTotal == Random){
+            victory();
+          }
+          else if ( userTotal > Random){
+            loser();
+          }
+    });   
+  }); 
